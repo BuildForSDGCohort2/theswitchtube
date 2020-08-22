d31dae9da930675a742f700c958ae4536eb9c619
freezy <awuyadanielz@gmail.com>
2020-08-22 13:34:41 +0100

    changes

--- theswitch/urls.py
+++ theswitch/urls.py
@@ -8,6 +8,5 @@ app_name = 'theswitch'
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('<int:post_id>', views.singlepost, name='singlepost'),
     path('home', views.home, name='home')
 ]
